deafc3bbdd96a123806c1b32a6c0461d68937f69
lithanohasheUCT <110478873+lithanohasheUCT@users.noreply.github.com>
2024-05-07 13:51:23 +0200

    Emulated Switches and Output LED
    Prepared space for APP2 (more advanced implementation)

--- Verilog/xor_encrypter.v
+++ Verilog/xor_encrypter.v
@@ -1,21 +1,41 @@
 module xor_encrypter (
     // Inputs
-    input wire clk,         // Operating Clock
-    input wire[2:0] shift,  // Number of bits to shift key by
-    input wire[7:0] key,    // Key for encryption
-    input wire[7:0] din,    // Data byte to encrypt
+    input wire clk,                 // Operating Clock
+    input wire[2:0] shift,          // Number of bits to shift key by
+    input wire[7:0] key,            // Key for encryption
+    input wire[7:0] din,            // Data byte to encrypt
+    input wire start_reset,         // Start/Reset switch
+    input wire xor_enable,          // XOR Encryption switch
+    input wire improved_encrypt_enable, // Improved Encryption module switch
+	input wire last_data,
 
     // Outputs
-    output reg[7:0] dout    // Encrypted data byte
+    output reg[7:0] dout,           // Encrypted data byte
+    output reg led_complete         // LED for completed state
     );
 
     // Local variables
-    reg [7:0] shifted_key;  // Key shifted by number of bits
-
+    reg [7:0] shifted_key;  			  // Key shifted by number of bits
+	
     always @(posedge clk) begin
         // Shift key to the left with wrap
         shifted_key <= (key << shift) | (key >> (8 - shift));
-        dout <= din ^ shifted_key;
+		
+		// Perform XOR encryption based on switch settings
+        if (start_reset) begin
+            dout <= 8'h00;  // Reset output when start/reset is active
+            led_complete <= 0;
+        end else if (xor_enable) begin
+            dout <= din ^ shifted_key;
+            led_complete <= 0;
+			if (last_data) begin
+				led_complete	<=	1;
+			end
+        end else if (improved_encrypt_enable) begin
+            // Implement improved encryption module
+            // ...
+            led_complete <= 1;  // Set LED to indicate completion
+        end
     end
-
+	
 endmodule
--- Verilog/xor_encrypter_tb.v
+++ Verilog/xor_encrypter_tb.v
@@ -6,9 +6,14 @@ module xor_encrypter_tb;
     reg[2:0] shift = 0;
     reg[7:0] key = 0;
     reg[7:0] din = 0;
+    reg start_reset = 0;
+    reg xor_enable = 0;
+    reg improved_encrypt_enable = 0;
+	reg last_data = 0;
     
     // Outputs
     wire[7:0] dout;
+    wire led_complete;
 
     // Instantiate xor_encrypter module
     xor_encrypter U0 (
@@ -16,7 +21,12 @@ module xor_encrypter_tb;
         .shift(shift),
         .key(key),
         .din(din),
-        .dout(dout)
+        .start_reset(start_reset),
+        .xor_enable(xor_enable),
+        .improved_encrypt_enable(improved_encrypt_enable),
+		.last_data(last_data),
+        .dout(dout),
+        .led_complete(led_complete)
     );
 
     always begin
@@ -27,20 +37,30 @@ module xor_encrypter_tb;
         shift = 0;
         key = 0;
         din = 0;
+        start_reset = 0;
+        xor_enable = 0;
+        improved_encrypt_enable = 0;
     
 
-        $display("\t\ttime,\tclk,\tkey,\t\tshift,\tdin,\t\tdout");
-        $monitor("%d,\t%b\t,%b,\t%d,\t%b,\t%b", $time, clk, key, shift, din, dout);
+        $display("\t\ttime,\tclk,\tkey,\t\tshift,\tdin,\t\tstart_reset,\txor_enable,\timproved_enable,\tdout,\t\tled_complete");
+        $monitor("%d,\t%b\t,%b,\t%d,\t%b,\t\t%b,\t\t%b,\t\t%b,\t\t%b,\t\t%b", $time, clk, key, shift, din, start_reset, xor_enable, improved_encrypt_enable, dout, led_complete);
         
         #10 din = 8'hFF;
         #10 key = 8'b10101000;
-        #10 shift = 1;
+        $display("TESTING RESET");
+		#10 start_reset = 1;
+		#10 start_reset = 0;
+		$display("TESTING SIMPLER CIPHER");
+		#10 xor_enable = 1;
+		#10 shift = 1;
         #10 shift = 2;
         #10 shift = 3;
         #10 shift = 4;
         #10 shift = 5;
         #10 shift = 6;
         #10 shift = 7;
+		#10 last_data = 1;
+		#10 xor_enable = 0;
 
         // End the simulation
         #10 $finish;
